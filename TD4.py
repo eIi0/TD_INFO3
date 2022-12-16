8a74fe1cc8a359896cf7362f545fc4da49d79d11
eli0 <eliotommasini.ET@gmail.com>
2022-12-16 14:11:07 +0100

    Revert "modif td4 ==> 2h de taf perdu"
    This reverts commit b9f3eb7e50b26857c064e3255c4bbc3836ef9c86.

--- TD4.py
+++ TD4.py
@@ -1,6 +1,3 @@
-import random
-
-
 class Matrice2D():
     def __init__(self, nbLignes, nbColonnes):
         self.nbLignes = nbLignes
@@ -18,33 +15,3 @@ class Matrice2D():
             for val in lig:
                 print(f"{val:.2f} ", end='')
             print("")
-    
-    def randomInit(self, etendue):
-        random.seed()
-
-        for i in range(self.nbLignes):
-            for j in range(self.nbColonnes):
-                    self.Matrice[i][j] = random.uniform(0.0,etendue)
-
-
-
-class MatriceRotation(Matrice2D):
-    def __init__(self, axe,angle=0):
-        self.axe=axe
-        self.angle=angle
-
-        # if
-
-
-
-    def affiche(self):
-        Matrice2D.affiche(self)
-
-    # def multiplieMatrice(self,MatriceRotationA, axe):
-        
-
-if __name__ == "__main__":
-    m = Matrice2D(2,3)
-    m.affiche()
-    m.randomInit(100)
-    m.affiche()
